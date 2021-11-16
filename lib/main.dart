f829f147e58494c69c5f8ee8361bb11bd68f7829
Eric Seidel <eseidel@google.com>
2021-11-16 12:12:37 -0800

    as small as I can get it

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+
+// Reproduction instructions
+// Launch in iOS simulator
+// Click on the top FormField, type "www😄😄😄", will require clicking on
+// soft keyboard for ascii, switching to emoji, then clicking there.
+// Click on the second field, type "www😄" and iOS keyboard will hang!
 
 void main() async {
   runApp(MaterialApp(
-    home: ChangeNotifierProvider(
-      create: (context) => BoardEditorViewModel(),
-      builder: (context, child) => Scaffold(
-        appBar: AppBar(),
-        body: Consumer<BoardEditorViewModel>(builder: (context, editor, child) {
-          return Column(
-            children: [TextFormField(), const NodeItem()],
-          );
-        }),
+    home: Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [TextFormField(), const NodeItem()],
       ),
     ),
   ));
 }
 
-class NodeItem extends StatelessWidget {
+class NodeItem extends StatefulWidget {
   const NodeItem({
     Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Consumer<BoardEditorViewModel>(
-      builder: (context, node, child) {
-        return TextField(
-          controller: node.controller,
-        );
-      },
-    );
-  }
+  State<NodeItem> createState() => _NodeItemState();
 }
 
-class BoardEditorViewModel with ChangeNotifier {
-  // MARKER SEEMS REQUIRED?
-  static const kMarker = '\u0000';
+class _NodeItemState extends State<NodeItem> {
+  // This text seems to be required, unclear why:
+  late final controller = TextEditingController(text: '\u0000');
 
-  late final controller = TextEditingController(text: kMarker);
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      // Originally Provider was causing the rebuilds, but this seem to
+      // work too:
+      onChanged: (_) => setState(() {}),
+    );
+  }
 }
